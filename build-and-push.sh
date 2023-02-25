7cdeab2e5059e3bd76a11798a75543393f822963
JohannesIBK <mail@greuter.dev>
2023-02-25 13:01:12 +0100

    fikx build script

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,2 +1,3 @@
 docker build . -t plyoox/notificator
+docker tag plyoox/notificator plyoox.registry.jetbrains.space/p/plyoox/plyoox/notificator
 docker push plyoox.registry.jetbrains.space/p/plyoox/plyoox/notificator
